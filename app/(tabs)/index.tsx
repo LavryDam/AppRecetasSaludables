03da4723ac99048729a618e489a63621f59c191a
Dina Martínez Ruiz <Dinaalmeria@gmail.com>
2025-02-17 12:43:22 +0100

    fix: install lint and prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-const icon = require('../../assets/images/ImagenRecetas.jpg');
+const icon = require("../../assets/images/ImagenRecetas.jpg");
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Image source={icon} style={{ width: 410, height: 730 }} />
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -15,8 +15,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
