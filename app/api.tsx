d4bca5cdf4c940a5f1061543893cd5a8fe2b30d2
Dina Martínez Ruiz <Dinaalmeria@gmail.com>
2025-03-03 13:34:23 +0100

    fix: api

---
+++ app/api.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+const API_KEY = "1";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  params: {
+    apiKey: API_KEY,
+  },
+});
+
+export const fetchRecipes = async (query: any) => {
+  try {
+    const response = await api.get("/complexSearch", { params: { query } });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    throw error;
+  }
+};
